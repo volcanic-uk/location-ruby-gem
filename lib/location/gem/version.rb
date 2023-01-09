4c5cdd7047e462c579b880fa37c37cbd4ac5f14a
Mark McKen <markmckennedy@yahoo.com>
2023-01-09 10:31:48 +0000

    Initial commit

---
+++ lib/location/gem/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Location
+  module Gem
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/location/gem_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Location::Gem do
+  it "has a version number" do
+    expect(Location::Gem::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
