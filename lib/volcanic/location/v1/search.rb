239662c251d293fe847a00066f4dcb49fc8df751
matthew-carter-access <matthew.carter@theaccessgroup.com>
2023-02-03 14:29:44 +0000

    VOL-2802 Add pagination support

--- lib/volcanic/location/v1/search.rb
+++ lib/volcanic/location/v1/search.rb
@@ -7,29 +7,25 @@ class Volcanic::Location::V1::Search
   include Volcanic::Location::ConnectionHelper
 
   API_PATH = "api/v1/locations"
-  attr_reader("locations")
+  attr_reader("locations", "pagination")
 
   # Initialiser with filter as param
   # Then call search with the filter
   def initialize(**filter)
-    write_attr(filter)
+    search(filter)
   end
 
   private
 
   def search(filter)
     res = conn.get(API_PATH) do |req|
-      # req.headers = { "Content-Type" => "multipart/form-data" }
       req.body = { 'query': filter }
     end
 
-    res.body[:locations]
+    self.locations = res.body[:locations]
+    self.pagination = res.body[:pagination]
     # TODO: create Location objects
   end
 
-  attr_writer("locations")
-
-  def write_attr(filter)
-    self.locations = search(filter)
-  end
+  attr_writer("locations", "pagination")
 end
