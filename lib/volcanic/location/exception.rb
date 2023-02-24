57e44119355685f843f6b7c25cf271cbed24620d
matthew-carter-access <matthew.carter@theaccessgroup.com>
2023-02-24 16:50:19 +0000

    VOL-2798 Added exception middleware and classes

--- lib/volcanic/location/exception.rb
+++ lib/volcanic/location/exception.rb
@@ -7,5 +7,14 @@ module Volcanic
     class MissingConfiguration < LocationError; end
 
     class ServerError < LocationError; end
+
+    # TODO: find new name for this
+    class MiddleError < LocationError; end
+
+    class LocationNotFound < MiddleError; end
+
+    class Forbidden < MiddleError; end
+
+    class S3SignedUrlError < MiddleError; end
   end
 end
--- volcanic-location.gemspec
+++ volcanic-location.gemspec
@@ -24,6 +24,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "faraday", "~> 1.10.3"
   spec.add_dependency "faraday_middleware", "~> 1.0"
+  spec.add_dependency "activesupport", "~> 6.0.3.7"
 
   spec.add_development_dependency "rake", "~> 13.0"
   spec.add_development_dependency "rspec", "~> 3.0"
