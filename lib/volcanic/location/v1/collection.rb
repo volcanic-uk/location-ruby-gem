fb8fae83587c8752375dfc9aaab78832cb7f6a1a
Farid <ahmadfaridulazmi@gmail.com>
2023-03-17 23:04:49 +0800

    refactor

--- lib/volcanic/location/v1/collection.rb
+++ lib/volcanic/location/v1/collection.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
-class Volcanic::Location::V1::Collection < Array
+module Volcanic::Location::V1
+  class Collection < Array
+    attr_accessor :page, :page_size
 
-  attr_accessor :page, :page_size
-
-  def initialize(array, page: nil, page_size: nil)
-    @page = page
-    @page_size = page_size
-    super array
-  end
-
-  def self.for_locations(collection, **args)
-    array = collection.map do |item|
-      Volcanic::Location::V1::Location.new(**item)
+    def initialize(array, page: nil, page_size: nil)
+      @page = page
+      @page_size = page_size
+      super array
     end
 
-    new(array, **args)
+    def self.for_locations(collection, **args)
+      array = collection.map do |item|
+        Volcanic::Location::V1::Location.new(**item)
+      end
+
+      new(array, **args)
+    end
   end
 end
